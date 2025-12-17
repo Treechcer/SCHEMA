d293b2161aa6a9c90607934568cdfd4b5d0456bb
Treechcer <treechcer@gmail.com>
2025-12-17 20:16:09 +0100

    added love2D as starter you can use

---
+++ builds/love2d/main.lua
@@ -0,0 +1,17 @@
+love = require("love")
+
+function love.load()
+    love.graphics.setDefaultFilter("nearest", "nearest") --this is used for pixel art games
+end
+
+function love.draw()
+
+end
+
+function love.update(dt)
+
+end
+
+function love.mousepressed(x, y, button, istouch, presses)
+
+end
